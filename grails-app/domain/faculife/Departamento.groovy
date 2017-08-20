44498c6121dd2efdc1402440c81ee44846dcc0e3
Gaston <gnp.gaston@gmail.com>
2017-08-20 18:27:32 -0300

    Codigo de departamento como unique

--- grails-app/domain/faculife/Departamento.groovy
+++ grails-app/domain/faculife/Departamento.groovy
@@ -7,7 +7,7 @@ class Departamento {
     static hasMany = [materias: Materia]
 
     static constraints = {
-        codigo required: true, nullable: false, blank: false
+        codigo unique: true, required: true, nullable: false, blank: false
         nombre required: true, nullable: false, blank: false, minSize: 3, maxSize: 60
     }
 }
