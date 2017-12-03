2dcca16f725dea19427d1ef0dbe0cade75e18a81
alex_92 <ale.92xx@yahoo.com>
2017-12-02 23:08:45 -0300

    agregue test de alumno se inscribe a una carrera, pero falta el assert

--- grails-app/domain/faculife/Curso.groovy
+++ grails-app/domain/faculife/Curso.groovy
@@ -1,10 +1,12 @@
 package faculife
 
 class Curso {
+    String description
     static belongsTo = Alumno
 
     static hasMany = [alumnos: Alumno]
 
     static constraints = {
+      description nullable: false, blank: false
     }
 }
--- src/test/groovy/faculife/CarreraSpec.groovy
+++ src/test/groovy/faculife/CarreraSpec.groovy
@@ -1,13 +1,13 @@
 package faculife
 
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import grails.test.hibernate.HibernateSpec
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Carrera)
-class CarreraSpec extends Specification {
+class CarreraSpec extends HibernateSpec {
 
     def setup() {
     }
@@ -15,4 +15,26 @@ class CarreraSpec extends Specification {
     def cleanup() {
     }
 
+    def 'asignarle una carrera a un alumno' () {
+      when:
+          Carrera carrera = new Carrera(codigo: '8989'
+              , nombre: 'Analisis de Sistemas')
+          Materia analisis = new Materia(nombre: 'analisis matematico II'
+              , codigo: '81.01')
+          Curso curso = new Curso(description: '1B Acero')
+          Alumno alumno = new Alumno(padron: '123456'
+              , nombres: 'Gaston'
+              , apellidos: 'Perez'
+              , numeroDocumento: '34114043'
+              , fechaNacimiento: new Date())
+
+          carrera.save()
+          alumno.save()
+          analisis.addToCursos(curso)
+          carrera.adToMaterias(analisis)
+          alumno.addToCursos(curso)
+
+      //then:
+
+    }
 }
